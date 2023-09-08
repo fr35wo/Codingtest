f9d8bf3eb9ed9169e3e10505ae5dd58ac25abb11
홍석현 <114850357+fr35wo@users.noreply.github.com>
2023-09-09 01:27:23 +0900

    Add files via upload

---
+++ Main_14582.java
@@ -0,0 +1,30 @@
+package bak;
+import java.util.*;
+
+public class Main_14582 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a[] = new int[9];
+        for(int i=0; i<9; i++)
+            a[i] = sc.nextInt();
+        
+        int b[] = new int[9];
+        for(int i=0; i<9; i++)
+            b[i] = sc.nextInt();
+        
+        int ulim = 0;
+        int stl = 0;
+        for(int i=0; i<9; i++) {
+            ulim += a[i];
+            if(ulim > stl) {
+                System.out.println("Yes");
+                return;
+            }
+            stl += b[i];
+        }
+        
+        System.out.println("No");
+    }
+}
