d39e4fe7e5b1d45e9d82abcbb0dd45c451b2ff40
seokhyeon <114850357+fr35wo@users.noreply.github.com>
2024-12-24 10:42:34 +0900

    [level 1] Title: K번째수, Time: 1.22 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,29 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-    	
-    	int[] answer = new int[commands.length];
-    	
-    	for(int m=0; m<commands.length; m++) {
-    		int i=0, j=0, k=0;
-    		
-    		for(int n=0; n<commands[m].length; n++) {
-    			if(n==0) {
-    				i = commands[m][n];
-    			} else if(n==1) {
-    				j = commands[m][n];
-    			}else if(n==2) {
-    				k = commands[m][n];
-    			}
-    		}
-    		
-    		int newArr[] = Arrays.copyOfRange(array, i-1, j);
-    		Arrays.sort(newArr);
-    		
-    		answer[m] = newArr[k-1];
-    		
-    	}
-    	
+        int[] answer = new int[commands.length];
+
+        for (int m = 0; m < commands.length; m++) {
+            int i = commands[m][0]; // 시작 인덱스 (1-based index)
+            int j = commands[m][1]; // 끝 인덱스 (1-based index)
+            int k = commands[m][2]; // k번째 값
+
+            // 복사할 배열의 길이 계산
+            int length = j - (i - 1);
+            int[] newArr = new int[length];
+
+            // System.arraycopy()를 사용하여 범위 복사
+            System.arraycopy(array, i - 1, newArr, 0, length);
+
+            // 복사된 배열 정렬
+            Arrays.sort(newArr);
+
+            // k번째 값 저장
+            answer[m] = newArr[k - 1];
+        }
+
         return answer;
     }
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 90.7 MB, 시간: 0.44 ms
+메모리: 84.9 MB, 시간: 1.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 24일 10:32:57
+2024년 12월 24일 10:42:32
 
 ### 문제 설명
 
